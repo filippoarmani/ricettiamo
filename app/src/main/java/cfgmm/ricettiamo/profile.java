4eeb94cef4de7091155ce67d5bb2a5edf960a790
ccyrenee <c.abjelina@campus.unimib.it>
2022-11-10 18:32:28 +0100

    add profile

---
+++ app/src/main/java/cfgmm/ricettiamo/profile.java
@@ -0,0 +1,14 @@
+package cfgmm.ricettiamo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
