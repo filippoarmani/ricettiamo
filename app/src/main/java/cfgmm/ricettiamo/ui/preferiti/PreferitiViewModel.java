2cc8ba5fa41ec9c858fe44225b388c7168964e8d
filippoarmani <102678173+filippoarmani@users.noreply.github.com>
2022-11-11 11:11:15 +0100

    fragment e view model

---
+++ app/src/main/java/cfgmm/ricettiamo/ui/preferiti/PreferitiViewModel.java
@@ -0,0 +1,19 @@
+package cfgmm.ricettiamo.ui.preferiti;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PreferitiViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public PreferitiViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Preferiti fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
