71e3f8806c61a063fcbe9eed8711a287789f499a
mcolombo326 <102531894+mcolombo326@users.noreply.github.com>
2022-11-20 17:51:18 +0100

    font caratteri per schermata login e registrazione

---
+++ app/src/main/java/cfgmm/ricettiamo/frigorifero.java
@@ -0,0 +1,14 @@
+package cfgmm.ricettiamo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frigorifero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frigorifero);
+    }
+}
