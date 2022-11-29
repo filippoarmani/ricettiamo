db02b8438c454ba87882e84cf906e5a56ca8fc8f
filippoarmani <102678173+filippoarmani@users.noreply.github.com>
2022-11-29 16:20:39 +0100

    TextInputLayout, TextInputEditText Tentativo calendario per data di nascita

--- app/src/main/java/cfgmm/ricettiamo/LoginActivity.java
+++ app/src/main/java/cfgmm/ricettiamo/LoginActivity.java
@@ -6,38 +6,13 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
-
-    private EditText email;
-    private EditText password;
-
-    private Button forgot_p;
-    private Button login;
-    private Button sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
-
-        email = findViewById(R.id.l_email);
-        password = findViewById(R.id.l_password);
-
-        forgot_p = findViewById(R.id.l_passwordD);
-        login = findViewById(R.id.l_login);
-        sign_in = findViewById(R.id.l_registrati);
-
-        forgot_p.setOnClickListener(v -> {
-
-        });
-
-        login.setOnClickListener(v -> {
-
-        });
-
-        sign_in.setOnClickListener(v -> {
-
-        });
-
     }
 }
--- app/src/main/java/cfgmm/ricettiamo/RegistrationActivity.java
+++ app/src/main/java/cfgmm/ricettiamo/RegistrationActivity.java
@@ -4,20 +4,31 @@ import static android.text.TextUtils.isEmpty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText e_nome;
-    private EditText e_cognome;
-    private EditText e_dataNascita;
-    private EditText e_email;
-    private EditText e_phoneNumber;
-    private EditText e_password;
+    final Calendar myCalendar= Calendar.getInstance();
+
+    private TextInputLayout e_nome;
+    private TextInputLayout e_cognome;
+    private TextInputLayout e_dataNascita;
+    private TextInputLayout e_email;
+    private TextInputLayout e_phoneNumber;
+    private TextInputLayout e_password;
 
     private Button registrati;
 
@@ -26,36 +37,38 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_page);
 
-        e_nome = findViewById(R.id.r_nome);
-        e_cognome = findViewById(R.id.r_cognome);
-        e_dataNascita = findViewById(R.id.r_dataNascita);
-        e_email = findViewById(R.id.r_email);
-        e_phoneNumber = findViewById(R.id.r_telefono);
-        e_password = findViewById(R.id.r_password);
+        e_nome = findViewById(R.id.r_nome_layout);
+        e_cognome = findViewById(R.id.r_cognome_layout);
+        e_dataNascita = findViewById(R.id.r_date_layout);
+        e_email = findViewById(R.id.r_email_layout);
+        e_phoneNumber = findViewById(R.id.r_phone_layout);
+        e_password = findViewById(R.id.r_password_layout);
 
         registrati = findViewById(R.id.r_creaAccount);
 
+        DatePickerDialog.OnDateSetListener date = (view, year, month, day) -> {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH,month);
+            myCalendar.set(Calendar.DAY_OF_MONTH,day);
+            updateLabel();
+        };
+
+        e_dataNascita.getEditText().setOnClickListener(view ->
+                new DatePickerDialog(RegistrationActivity.this,date,
+                        myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        );
+
         registrati.setOnClickListener(v -> {
-            String nome = e_nome.getText().toString();
-            String cognome = e_cognome.getText().toString();
-            String dataNascita = e_dataNascita.getText().toString();
-            String email = e_email.getText().toString();
-            String numero = e_phoneNumber.getText().toString();
-            String password = e_password.getText().toString();
-
-            if(isEmpty(nome) || isEmpty(cognome) || isEmpty(dataNascita) || isEmpty(email) || isEmpty(numero) || isEmpty(password)) {
-                Snackbar.make(v, "The fields must not be empty", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-
-            if(!isDataOk(dataNascita)) {
-                Snackbar.make(v, "Wrong Date Format", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 
-    private boolean isDataOk(String dataNascita) {
-        return true;
+    private void updateLabel(){
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        e_dataNascita.getEditText().setText(dateFormat.format(myCalendar.getTime()));
     }
 }
