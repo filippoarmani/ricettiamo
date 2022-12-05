cff58f85611d76aa775eb044fb066d56c59fddf5
filippoarmani <102678173+filippoarmani@users.noreply.github.com>
2022-12-05 12:01:13 +0100

    Login e Registrazione (da finire)

--- app/src/main/java/cfgmm/ricettiamo/LoginActivity.java
+++ app/src/main/java/cfgmm/ricettiamo/LoginActivity.java
@@ -1,18 +1,102 @@
 package cfgmm.ricettiamo;
 
+import static android.content.ContentValues.TAG;
+import static android.text.TextUtils.isEmpty;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout email_layout;
+	private TextInputLayout password_layout;
+	private Button login;
+	private Button f_password;
+	private Button login_later;
+	private Button sign_in;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+        mAuth = FirebaseAuth.getInstance();
+
+        email_layout = findViewById(R.id.l_email_layout);
+        password_layout = findViewById(R.id.l_password_layout);
+
+        login = findViewById(R.id.l_login);
+        login_later = findViewById(R.id.btn_noLogIn);
+        sign_in = findViewById(R.id.l_registrati);
+        f_password = findViewById(R.id.l_passwordD);
+
+        login_later.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        sign_in.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), RegistrationActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        login.setOnClickListener(v -> {
+            String email = email_layout.getEditText().getText().toString().trim();
+            String password = password_layout.getEditText().getText().toString().trim();
+
+            if (!(isEmpty(email) || isEmpty(password))) {
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                        });
+            } else {
+                Snackbar.make(v, "The fields must not be empty", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            currentUser.reload();
+        }
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        Intent intent;
+
+        if (currentUser == null)
+            intent = new Intent(this, LoginActivity.class);
+        else
+            intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/cfgmm/ricettiamo/RegistrationActivity.java
+++ app/src/main/java/cfgmm/ricettiamo/RegistrationActivity.java
@@ -1,19 +1,21 @@
 package cfgmm.ricettiamo;
 
+import static android.content.ContentValues.TAG;
 import static android.text.TextUtils.isEmpty;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,6 +23,7 @@ import java.util.Locale;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     final Calendar myCalendar= Calendar.getInstance();
 
     private TextInputLayout e_nome;
@@ -36,6 +39,7 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_page);
+        mAuth = FirebaseAuth.getInstance();
 
         e_nome = findViewById(R.id.r_nome_layout);
         e_cognome = findViewById(R.id.r_cognome_layout);
@@ -60,15 +64,76 @@ public class RegistrationActivity extends AppCompatActivity {
         );
 
         registrati.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
+
+            String nome = e_nome.getEditText().getText().toString().trim();
+            String cognome = e_cognome.getEditText().getText().toString().trim();
+            String dataNascita = e_dataNascita.getEditText().getText().toString().trim();
+            String phoneNumber = e_phoneNumber.getEditText().getText().toString().trim();
+
+            String email = e_email.getEditText().getText().toString().trim();
+            String password = e_password.getEditText().getText().toString().trim();
+
+            if(checkOk(nome, cognome, dataNascita, phoneNumber, email, password)) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                        });
+            }  else {
+                Snackbar.make(v, "The fields must not be empty", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+
         });
     }
 
+    private boolean checkOk(String nome, String cognome, String dataNascita, String phoneNumber, String email, String password) {
+        if(isEmpty(nome) ||
+                isEmpty(cognome) ||
+                isEmpty(dataNascita) ||
+                isEmpty(phoneNumber) ||
+                isEmpty(email) ||
+            isEmpty(password))
+            return false;
+
+        return true;
+    }
+
     private void updateLabel(){
-        String myFormat="MM/dd/yy";
+        String myFormat="dd/MM/yyyy";
         SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
         e_dataNascita.getEditText().setText(dateFormat.format(myCalendar.getTime()));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        Intent intent;
+
+        if(currentUser == null)
+            intent = new Intent(this, RegistrationActivity.class);
+        else
+            intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+        finish();
     }
 }
