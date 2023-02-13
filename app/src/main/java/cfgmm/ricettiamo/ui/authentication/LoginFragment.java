835ea692cc1409031cffd98cc5a0ee499e148291
filippoarmani <102678173+filippoarmani@users.noreply.github.com>
2023-02-13 14:11:42 +0100

    Test new Google Login

--- app/src/main/java/cfgmm/ricettiamo/ui/authentication/LoginFragment.java
+++ app/src/main/java/cfgmm/ricettiamo/ui/authentication/LoginFragment.java
@@ -18,13 +18,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -42,18 +51,18 @@ public class LoginFragment extends Fragment {
 
     private static final String TAG = LoginFragment.class.getSimpleName();
     private static final boolean USE_NAVIGATION_COMPONENT = true;
+    private static final int RC_SIGN_IN = 1;
 
-    private CircularProgressIndicator progressIndicator;
-
-    private ActivityResultLauncher<IntentSenderRequest> activityResultLauncher;
-    private ActivityResultContracts.StartIntentSenderForResult startIntentSenderForResult;
+    private LinearProgressIndicator progressIndicator;
 
     private TextInputLayout email_layout;
     private TextInputLayout password_layout;
 
+    private GoogleSignInClient mGoogleApiClient;
+
     private UserViewModel userViewModel;
-    private SignInClient oneTapClient;
-    private BeginSignInRequest signInRequest;
+
+    private SignInButton login_google;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -69,57 +78,12 @@ public class LoginFragment extends Fragment {
         IUserRepository userRepository = ServiceLocator.getInstance().getUserRepository();
         userViewModel = new ViewModelProvider(requireActivity(), new UserViewModelFactory(userRepository)).get(UserViewModel.class);
 
-        oneTapClient = Identity.getSignInClient(requireActivity());
-        signInRequest = BeginSignInRequest.builder()
-                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                        .setSupported(true)
-                        .build())
-                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                        .setSupported(true)
-                        // Your server's client ID, not your Android client ID.
-                        .setServerClientId(getResources().getString(R.string.default_web_client_id))
-                        // Only show accounts previously used to sign in.
-                        .setFilterByAuthorizedAccounts(false)
-                        .build())
-                // Automatically sign in when exactly one credential is retrieved.
-                .setAutoSelectEnabled(true)
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
                 .build();
 
-        startIntentSenderForResult = new ActivityResultContracts.StartIntentSenderForResult();
-
-        activityResultLauncher = registerForActivityResult(startIntentSenderForResult, activityResult -> {
-            if (activityResult.getResultCode() == Activity.RESULT_OK) {
-                Log.d(TAG, "result.getResultCode() == Activity.RESULT_OK");
-                try {
-                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(activityResult.getData());
-                    String idToken = credential.getGoogleIdToken();
-                    if (idToken !=  null) {
-                        userViewModel.signInGoogle(idToken);
-                        try {
-                            TimeUnit.SECONDS.sleep(1);
-                        } catch (InterruptedException e) {
-                            Snackbar.make(requireView(), e.getMessage(), Snackbar.LENGTH_SHORT).show();
-                        }
-
-                        userViewModel.getCurrentUserLiveData().observe(getViewLifecycleOwner(), result -> {
-                            progressIndicator.setVisibility(View.GONE);
-                            if(result.isSuccess()) {
-                                updateUI(userViewModel.isLoggedUser());
-                            } else {
-                                if(!userViewModel.isLoggedUser()) {
-                                    Result.Error error = (Result.Error) result;
-                                    Snackbar.make(requireView(), error.getMessage(), Snackbar.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                } catch (ApiException e) {
-                    Snackbar.make(requireActivity().findViewById(android.R.id.content),
-                            requireActivity().getString(R.string.unexpected_error),
-                            Snackbar.LENGTH_SHORT).show();
-                }
-            }
-        });
+        mGoogleApiClient = GoogleSignIn.getClient(requireActivity(), gso);
     }
 
     @Override
@@ -135,12 +99,12 @@ public class LoginFragment extends Fragment {
         password_layout = view.findViewById(R.id.l_password_layout);
 
         Button login_password = view.findViewById(R.id.l_login);
-        Button login_google = view.findViewById(R.id.button_Login_Google);
+        login_google = view.findViewById(R.id.button_Login_Google);
         Button f_password = view.findViewById(R.id.l_forgotPassword);
         Button login_later = view.findViewById(R.id.btn_noLogIn);
         Button sign_up = view.findViewById(R.id.l_registration);
 
-        progressIndicator = view.findViewById(R.id.l_progress_circular);
+        progressIndicator = view.findViewById(R.id.progress);
 
         login_password.setOnClickListener(v -> {
             progressIndicator.setVisibility(View.VISIBLE);
@@ -156,7 +120,6 @@ public class LoginFragment extends Fragment {
                 }
 
                 userViewModel.getCurrentUserLiveData().observe(getViewLifecycleOwner(), result -> {
-                    progressIndicator.setVisibility(View.GONE);
                     if(result.isSuccess()) {
                         updateUI(userViewModel.isLoggedUser());
                     } else {
@@ -168,8 +131,8 @@ public class LoginFragment extends Fragment {
                 });
             } else {
                 Snackbar.make(requireView(), R.string.empty_fields, Snackbar.LENGTH_SHORT).show();
-                progressIndicator.setVisibility(View.GONE);
             }
+            progressIndicator.setVisibility(View.GONE);
         });
 
         f_password.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_forgotPasswordFragment));
@@ -180,24 +143,9 @@ public class LoginFragment extends Fragment {
 
         login_google.setOnClickListener(v -> {
             progressIndicator.setVisibility(View.VISIBLE);
-            oneTapClient.beginSignIn(signInRequest)
-                    .addOnSuccessListener(requireActivity(), result -> {
-                        Log.d(TAG, "onSuccess from oneTapClient.beginSignIn(BeginSignInRequest)");
-
-                        IntentSenderRequest intentSenderRequest =
-                                new IntentSenderRequest.Builder(result.getPendingIntent()).build();
-                        activityResultLauncher.launch(intentSenderRequest);
-
-                    })
-                    .addOnFailureListener(requireActivity(), e -> {
-                        // No saved credentials found. Launch the One Tap sign-up flow, or
-                        // do nothing and continue presenting the signed-out UI.
-                        Log.d(TAG, e.getLocalizedMessage());
-                        Snackbar.make(requireActivity().findViewById(android.R.id.content),
-                                requireActivity().getString(R.string.error_no_google_account_found_message),
-                                Snackbar.LENGTH_SHORT).show();
-                        progressIndicator.setVisibility(View.GONE);
-                    });
+            Intent signInIntent = mGoogleApiClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+            progressIndicator.setVisibility(View.GONE);
         });
     }
 
@@ -214,5 +162,45 @@ public class LoginFragment extends Fragment {
     public void onStart() {
         super.onStart();
         updateUI(userViewModel.isLoggedUser());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                if(account != null) {
+
+                    userViewModel.signInGoogle(account.getIdToken());
+
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        Snackbar.make(requireView(), e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+
+                    userViewModel.getCurrentUserLiveData().observe(getViewLifecycleOwner(), result2 -> {
+                        progressIndicator.setVisibility(View.GONE);
+                        if (result2.isSuccess()) {
+                            updateUI(userViewModel.isLoggedUser());
+                        } else {
+                            if (!userViewModel.isLoggedUser()) {
+                                Result.Error error = (Result.Error) result2;
+                                Snackbar.make(requireView(), error.getMessage(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Snackbar.make(requireView(), R.string.unexpected_error, Snackbar.LENGTH_SHORT).show();
+                }
+            } else {
+                Snackbar.make(requireView(), R.string.unexpected_error, Snackbar.LENGTH_SHORT).show();
+            }
+        }
     }
 }
